c28432d66d4722db8c0371fd07319c7cc8bdc366
bogdanbirsa <bogdan.birsa@yahoo.ro>
2019-07-21 20:03:39 +0300

    second-try

--- Program.cs
+++ Program.cs
@@ -1,87 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-
-/*Model a ZOO keeping in mind that:
-A zoo has a name and a list of animals
-At a zoo they can bring new animals and transfer animals to new zoos
-At a zoo the animals are daily fed with their favorite food, and each animal eats what it loves to eat.
-Each animal knows to eat by itself, and not all animals of the same type prefer the same type of food. Eg; a horse prefers carrots, and another horse prefers apples*/
-class Zoo
-
+﻿
+// Model a ZOO keeping in mind that:
+// A zoo has a name and a list of animals
+// At a zoo they can bring new animals and transfer animals to new zoos
+// At a zoo the animals are daily fed with their favorite food, and each animal eats what it loves to eat.
+// Each animal knows to eat by itself, and not all animals of the same type prefer the same type of food.Eg; a horse prefers carrots, and another horse prefers apples
+namespace ZooHomeWork
 {
-    public string NameOfZoo;
-    public Dictionary<string, string> animalsList = new Dictionary<string, string>();
-    public static string NameOfAnimal;
-    public string FavoriteFood;
-    public static int NrOfAnimals;
-    
     public class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            Zoo Zoo = new Zoo("GRADINA ZOOLOGICA");
-            Zoo.AddAnimal("leul", "carne cruda");
-            Zoo.AddAnimal("vulpea", "gaini");
-            Zoo.AddAnimal("caprioara", "iarba");
-            Zoo.AddAnimal("iepurele", "morcovi");
-            Zoo.AddAnimal("maimutele", "banane");
-            Zoo.AddAnimal("soarecii", "branza");
-            Zoo.PresentZoo();
-            Zoo.Transfer("maimutele");
-            Zoo.PresentZoo();
-        }
-    }
-    public Zoo(string name)
-    {
-        this.NameOfZoo = name;
-    }
+            Zoo newZoo = new Zoo("Zoo IASI");
+            newZoo.AllAnimals();
 
-    public Dictionary<string, string> AnimalsList //
-    {
-        get { return this.animalsList; }
-    }
+            var cal = new Cal("cal", AnimalFood.Iarba);
+            newZoo.AddAnimal(cal);
+            newZoo.AddAnimal(new Maimuta("maimuta", AnimalFood.Banane));
+            newZoo.AddAnimal(new Iepure("iepure", AnimalFood.Morcovi));
 
-    public void Eat() { }
+            newZoo.AllAnimals();
+            newZoo.RemoveAnimal(cal);
+            newZoo.AllAnimals();
 
-    static void AddAnimal()
-    {
-        NameOfAnimal = "";
-        NrOfAnimals = 0;
-    }
-
-    public void AddAnimal(string name, string food) //adaugam animale
-    {
-        NameOfAnimal = name;
-        this.FavoriteFood = food;
-        NrOfAnimals++;
-        this.animalsList.Add(name, food);
-    }
-
-    public static int GetNrOfAnimals() //aflam nr de animale
-    {
-        return NrOfAnimals;
-    }
-
-    public void Transfer(string animal) //transferam animale
-    {
-        if (this.AnimalsList.ContainsKey(animal))
-        {
-            Console.Write(this.AnimalsList.Remove(animal));
-            NrOfAnimals--;
-        }
-        else
-        {
-            Console.WriteLine("Nu avem animale in gradina zoologica");
-        }
-    }
-    public void PresentZoo()
-    {
-        int num = 1;
-        Console.WriteLine("In " + this.NameOfZoo + " sunt " + NrOfAnimals + " animale dupa cum urmeaza: ");
-        foreach (var animal in this.AnimalsList)
-        {
-            Console.WriteLine(num + ". " + animal.Key + ", mananca " + animal.Value);
-            num++;
+            newZoo.FeedAnimals();
         }
     }
 }
---
+++ Zoo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooHomeWork
+{
+    public class Zoo
+    {
+        public Zoo(string name)
+        {
+            this.name = name;
+        }
+
+        public List<Animal> animallist = new List<Animal>();
+        public string name;
+        public void AllAnimals()
+        {
+            if (animallist.Count == 0)
+            {
+                Console.WriteLine($"Initial nu sunt animale in {name}.");
+                return;
+            }
+
+            StringBuilder listOfAnimals = new StringBuilder();
+
+            foreach (var animal in animallist)
+            {
+                listOfAnimals.Append(animal.Name + ", ");
+            }
+
+            string listOfAnimalsTemp = listOfAnimals.ToString().Substring(0, listOfAnimals.Length - 2);
+
+            Console.WriteLine($"{name} are urmatoarele animale: {listOfAnimalsTemp}.");
+        }
+
+        public void AddAnimal(Animal animal)
+        {
+            animallist.Add(animal);
+            Console.WriteLine($"{animal.Name} adaugat(a).");
+        }
+
+        public void RemoveAnimal(Animal animal)
+        {
+            animallist.Remove(animal);
+            Console.WriteLine($"{animal.Name} eliminat(a).");
+        }
+
+        public void FeedAnimals()
+        {
+            foreach (var animal in animallist)
+            {
+                animal.Eat();
+            }
+        }
+    }
+}
